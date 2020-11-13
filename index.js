38baebc2ca9dbfeb54fce0aabcdec66a66416406
quinterobr <brahianquintero7@gmail.com>
2020-11-13 18:34:20 -0500

    carpeta operaciones, archivo operaciones y las llame desde el index

---
+++ index.js
@@ -0,0 +1,6 @@
+const operaciones = require("./operaciones/operaciones.js");
+
+console.log(operaciones.suma(4, 2));
+console.log(operaciones.resta(4, 2));
+console.log(operaciones.mult(4, 2));
+console.log(operaciones.div(4, 2));
---
+++ operaciones/operaciones.js
@@ -0,0 +1,6 @@
+module.exports = {
+    suma(a, b) { return a + b },
+    resta(a, b) { return a - b },
+    mult(a, b) { return a * b },
+    div(a, b) { return a / b }
+}
