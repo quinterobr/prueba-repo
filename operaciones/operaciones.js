adff1260ef1595963038cb8c7449c1800178a55d
quinterobr <brahianquintero7@gmail.com>
2020-11-13 19:10:04 -0500

    le puse una condicion a la funcion suma

--- operaciones/operaciones.js
+++ operaciones/operaciones.js
@@ -1,5 +1,7 @@
 module.exports = {
-    suma(a, b) { return a + b },
+    suma(a, b) {
+        return a == 0 ? b : a + b;
+    },
     resta(a, b) { return a - b },
     mult(a, b) { return a * b },
     div(a, b) { return a / b }
