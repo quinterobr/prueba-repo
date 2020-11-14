3c077c89e98ee81db24e6c4f898f3908e6fce9a0
quinterobr <brahianquintero7@gmail.com>
2020-11-13 19:43:30 -0500

    modifique la operacion resta

--- operaciones/operaciones.js
+++ operaciones/operaciones.js
@@ -2,7 +2,15 @@ module.exports = {
     suma(a, b) {
         return a == 0 ? b : a + b;
     },
-    resta(a, b) { return a - b },
+    resta(a, b) {
+        if (a < b) {
+            let c = a;
+            b = a;
+            b = c;
+            return a - b;
+        }
+        return a - b;
+    },
     mult(a, b) { return a * b },
     div(a, b) { return a / b }
 }
